a65efc466712e259a9a47b6ef7987652d999d7a9
Dinu Blanovschi <dinu.blanovschi@criptext.com>
2021-10-26 15:45:30 +0300

    Test rust-setup

--- default.nix
+++ default.nix
@@ -1,5 +1,7 @@
 { pkgs ? import <nixpkgs> { overlays = [(import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"))] } }
 
 {
+  inherit pkgs;
+
   mkShell = import ./nix/shell.nix pkgs;
 }
